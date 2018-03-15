3f4bd40659e11737035303dfb5121b1a88b97fbe
bjornt16 <bjornt16@ru.is>
2018-03-15 08:16:36 +0000

    Added SceneLoader Class and LoadingScreenUI now with fade in and out.

---
+++ Assets/Scripts/LoadingScreenUI.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class UILoadingScreen : MonoBehaviour
+{
+    //references to all of loadin
+    public UnityEngine.UI.RawImage loadScreen;
+    public GameObject loadingBarPanel;
+    public UnityEngine.UI.Slider LoadingBar;
+    public TextMeshProUGUI LoadingText;
+
+    public bool ready;
+    public float fadeTime = .8f;
+
+    public void SetPercentage(float progress)
+    {
+        if (!loadingBarPanel.activeInHierarchy)
+        {
+            loadingBarPanel.SetActive(true);
+        }
+
+        LoadingBar.value = progress;
+        LoadingText.text = progress * 100f + "%";
+
+        if(progress == 1f)
+        {
+            Destroy(true);
+        }
+    }
+
+    IEnumerator Fade(bool fadeIn, bool destroy = false)
+    {
+        ready = false;
+
+        if (loadingBarPanel.activeInHierarchy)
+        {
+            yield return new WaitForSeconds(.1f);
+            loadingBarPanel.SetActive(false);
+        }
+
+        float alphaMin = 0;
+        float alphaMax = 255;
+        float alpha = fadeIn ? alphaMin : alphaMax;
+        float time = 0;
+
+        loadScreen.color = new Color(loadScreen.color.r, loadScreen.color.g, loadScreen.color.b, alpha);
+
+        while(fadeIn ? alpha < alphaMax : alpha > alphaMin)
+        {
+            alpha = Mathf.Lerp(fadeIn ? alphaMin : alphaMax, fadeIn ? alphaMax : alphaMin, time / fadeTime);
+            loadScreen.color = new Color(loadScreen.color.r, loadScreen.color.g, loadScreen.color.b, alpha / alphaMax);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        if (destroy)
+        {
+            Destroy();
+        }
+
+        ready = true;
+    }
+
+    private void Awake()
+    {
+        StartCoroutine(Fade(true));
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void Destroy(bool fade = false)
+    {
+        if (fade)
+        {
+            StartCoroutine(Fade(false, true));
+        }
+        else
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Linq;
+
+public class SceneLoader : SingletonMB<SceneLoader>
+{
+    //a variable to hold a reference to the AsyncOperation
+    private AsyncOperation async;
+
+
+    //variables to hold references for the loadingscreen prefab, and it's instance.
+    public UILoadingScreen loadingUIPrefab;
+    private UILoadingScreen loadingUIinstance;
+
+    public override void CopyValues(SceneLoader copy)
+    {
+        if(copy.loadingUIPrefab != null)
+        {
+            loadingUIPrefab = copy.loadingUIPrefab;
+        }
+
+    }
+
+    IEnumerator LoadScene(string scene)
+    {
+        Debug.Log("scene: " + scene);
+        if (scene == "")
+            yield break;
+
+        //instantiate the loading screen, and pause momentarily while the screen fades in.
+        loadingUIinstance = Instantiate(loadingUIPrefab) as UILoadingScreen;
+        while (!loadingUIinstance.ready)
+        {
+            yield return null;
+        }
+
+        async = SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
+        if (async == null)
+        {
+            loadingUIinstance.Destroy();
+            yield break;
+        }
+
+        //update progress bar as scene loads.
+        while (!async.isDone)
+        {
+            loadingUIinstance.SetPercentage(async.progress);
+            yield return null;
+        }
+        //scene has finished loading, Get rid of the loading screen. (Destroy(True) fades out before destroying)
+        loadingUIinstance.SetPercentage(1f);
+
+        //unload the old Scene.
+        async = SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    public void Load(string scene)
+    {
+        Camera.main.GetComponent<AudioListener>().enabled = false;
+        StartCoroutine(LoadScene(scene));
+    }
+}
