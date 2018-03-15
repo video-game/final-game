6c42a43e1cd1a9e66843cc19bafba4c49b196b0f
Hallur A. Atlason <halluratlason@gmail.com>
2018-03-15 02:41:17 +0000

    Implemented movement, facing, hand, and shooting demo projectile

---
+++ Assets/Scripts/DemoProjectileScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoProjectileScript : MonoBehaviour
+{
+	[SerializeField]
+	private float lifetime;
+
+	private void Awake()
+	{
+		StartCoroutine(DestroyIn(lifetime));
+	}
+
+	private IEnumerator DestroyIn(float seconds)
+	{
+		yield return new WaitForSeconds(seconds);
+		Destroy(gameObject);
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		Destroy(gameObject);
+	}
+}
