2b0cf5692112ad57a641725751f4a661501f2838
bjornt16 <bjornt16@ru.is>
2018-03-17 06:43:02 +0000

    Simple script to test out the agent (temp)

---
+++ Assets/Scripts/NavMeshAgentDemo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshAgentDemo : MonoBehaviour {
+
+    NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.Log(agent);
+
+            agent.SetDestination(ray.GetPoint(1));
+        }
+    }
+
+}
