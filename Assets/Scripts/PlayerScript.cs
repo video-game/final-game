64f42f117713224f3682d1fdbef7b84d88263fef
ValgardurRI <valgardur16@ru.is>
2018-03-18 15:58:04 +0000

    fixed projectiles not colliding with the environment

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -23,7 +23,7 @@ public class PlayerScript : MonoBehaviour
 	private Vector2 centerpointOffset;
 
 	[SerializeField]
-	private Rigidbody2D demoProjectile; // prefab
+	private Rigidbody demoProjectile; // prefab
 
 	private Animator animator;
 	private Rigidbody body;
@@ -107,7 +107,7 @@ public class PlayerScript : MonoBehaviour
 	private void shootDemoProjectile()
 	{
 		var direction = calculateDirectionToMouse();
-		var clone = Instantiate(demoProjectile, hand.position, Quaternion.identity);
+		var clone = Instantiate(demoProjectile, new Vector3(hand.position.x, 0, hand.position.z), demoProjectile.transform.rotation);
 		clone.velocity = 9 * direction;
 	}
 
@@ -137,15 +137,15 @@ public class PlayerScript : MonoBehaviour
 	{
 		var directionVector = calculateDirectionToMouse();
 		var x = directionVector.x;
-		var y = directionVector.y;
+		var z = directionVector.z;
 
 		Utilities.Direction direction;
 
-		if (x < 0 && Mathf.Abs(x) > Mathf.Abs(y))
+		if (x < 0 && Mathf.Abs(x) > Mathf.Abs(z))
 			direction = Utilities.Direction.Left;
-		else if (x > 0 && x > Mathf.Abs(y))
+		else if (x > 0 && x > Mathf.Abs(z))
 			direction = Utilities.Direction.Right;
-		else if (y > 0 && y > Mathf.Abs(x))
+		else if (z > 0 && z > Mathf.Abs(x))
 			direction = Utilities.Direction.Up;
 		else
 			direction = Utilities.Direction.Down;
@@ -153,18 +153,18 @@ public class PlayerScript : MonoBehaviour
 		animator.SetInteger("Direction", (int)direction);
 	}
 
-	private Vector2 calculateDirectionToMouse()
+	private Vector3 calculateDirectionToMouse()
 	{
-		var mousePosition = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		var playerPosition = (Vector2)transform.position + centerpointOffset;
+		var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		var playerPosition = transform.position + new Vector3(centerpointOffset.x, 0, centerpointOffset.y);
 		var direction = (mousePosition - playerPosition).normalized;
 		return direction;
 	}
 
 	private void moveHandTowardsMouse()
 	{
-		var playerPosition = (Vector2)transform.position + centerpointOffset;
-		var direction = calculateDirectionToMouse();
+		Vector3 playerPosition = transform.position + new Vector3(centerpointOffset.x, 0, centerpointOffset.y);
+		Vector3 direction = calculateDirectionToMouse();
 		var handPosition = playerPosition + (direction * handRadius);
 		hand.position = handPosition;
 	}
