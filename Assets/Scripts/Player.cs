8f884f2dc7ed452ad19c40fce7604e77b7f93f97
bjornt16 <bjornt16@ru.is>
2018-03-19 05:19:26 +0000

    Refactored Player
    Player no longer uses hardcoded input handling.
    Input handling moved to InputManager.
    Movement & dash is now done with NavMesh (for consistency and now we only need mesh colliders on walls for projectiles)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,171 +1,94 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
-	[SerializeField]
-	private float speed;
+    [SerializeField]
+    private float speed;
 
-	[SerializeField]
-	private float dashSpeed;
+    [SerializeField]
+    private float dashSpeed;
 
-	[SerializeField]
-	private float dashDuration;
+    [SerializeField]
+    private float dashDuration;
 
-	[SerializeField]
-	private float dashCooldown;
+    [SerializeField]
+    private float dashCooldown;
 
-	[SerializeField]
-	private float handRadius;
+    [SerializeField]
+    private Rigidbody demoProjectile; // prefab
 
-	[SerializeField]
-	private Vector2 centerpointOffset;
+    private Animator animator;
+    private Rigidbody body;
+    private Transform hand;
 
-	[SerializeField]
-	private Rigidbody demoProjectile; // prefab
+    private bool dashOnCooldown;
+    private bool isWalking;
 
-	private Animator animator;
-	private Rigidbody body;
-	private Transform hand;
+    private NavMeshAgent agent;
 
-	private bool fireKeyDown;
-	private bool dashKeyDown;
-	private bool dashOnCooldown;
-	private bool movementInputEnabled = true;
-	private bool isWalking;
+    private Vector3 aimDirection;
 
-	private void Awake()
-	{
-		animator = GetComponent<Animator>();
-		body = GetComponent<Rigidbody>();
-		hand = transform.Find("Hand");
-	}
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+        body = GetComponentInChildren<Rigidbody>();
+        hand = transform.Find("Hand");
+        agent = GetComponent<NavMeshAgent>();
+    }
 
-	private void Update()
-	{
-		handleMovementInput();
-		handleDashInput();
-		handleFireInput();
-		faceTowardsMouse();
-		moveHandTowardsMouse();
-	}
+    //player move function
+    public void Move(float horizontal, float vertical)
+    {
+        isWalking = (horizontal != 0 || vertical != 0);
+        animator.SetBool("IsWalking", (horizontal != 0 || vertical != 0));
 
-	private void handleFireInput()
-	{
-		if (Input.GetAxisRaw("Fire1") != 0)
-		{
-			if (!fireKeyDown)
-			{
-				shootDemoProjectile();
-				fireKeyDown = true;
-			}
-		}
-		else
-			fireKeyDown = false;
-	}
+        Vector3 movePos = new Vector3(transform.position.x + horizontal, 0, transform.position.z + vertical);
+        agent.SetDestination(movePos);
+    }
 
-	private void handleDashInput()
-	{
-		if (Input.GetAxis("Dash") != 0)
-		{
-			if (!dashKeyDown)
-			{
-				if (!dashOnCooldown)
-				{
-					StartCoroutine(dash());
-					if (dashCooldown > 0f)
-						StartCoroutine(startDashCooldown());
-				}
-				dashKeyDown = true;
-			}
-		}
-		else
-			dashKeyDown = false;
-	}
+    //player dash function
+    public void Dash()
+    {
+        if (!dashOnCooldown)
+        {
+            StartCoroutine(DashCoroutine());
+            if (dashCooldown > 0f)
+                StartCoroutine(DashCooldown());
+        }
+    }
 
-	private IEnumerator dash()
-	{
-		if (isWalking)
-		{
-			movementInputEnabled = false;
-			Vector2 currentDirection = body.velocity.normalized;
-			body.velocity = new Vector3(currentDirection.x, 0 , currentDirection.y) * dashSpeed;
-			yield return new WaitForSeconds(dashDuration);
-			body.velocity = Vector2.zero;
-			movementInputEnabled = true;
-		}
-	}
+    private IEnumerator DashCoroutine()
+    {
+        Vector3 currentDirection = agent.velocity.normalized;
+        agent.velocity = new Vector3(currentDirection.x, 0, currentDirection.z) * dashSpeed;
+        yield return new WaitForSeconds(dashDuration);
+        agent.velocity = Vector2.zero;
+    }
 
-	private IEnumerator startDashCooldown()
-	{
-		dashOnCooldown = true;
-		yield return new WaitForSeconds(dashCooldown);
-		dashOnCooldown = false;
-	}
+    private IEnumerator DashCooldown()
+    {
+        dashOnCooldown = true;
+        yield return new WaitForSeconds(dashCooldown);
+        dashOnCooldown = false;
+    }
 
-	private void shootDemoProjectile()
-	{
-		var direction = calculateDirectionToMouse();
-		var clone = Instantiate(demoProjectile, new Vector3(hand.position.x, 0, hand.position.z), demoProjectile.transform.rotation);
-		clone.velocity = 9 * direction;
-	}
+    public void Shoot()
+    {
+        Rigidbody clone = (Rigidbody)Instantiate(demoProjectile, new Vector3(hand.position.x, 0, hand.position.z), demoProjectile.transform.rotation);
+        clone.velocity = 9 * aimDirection;
+    }
 
-	private void handleMovementInput()
-	{
-		if (movementInputEnabled)
-		{
-			var horizontalInput = Input.GetAxisRaw("Horizontal");
-			var verticalInput = Input.GetAxisRaw("Vertical");
+    public void AimInDirection(Vector3 direction)
+    {
+        aimDirection = direction;
 
-			var velocity = new Vector3();
-			velocity.x = horizontalInput * speed;
-			velocity.z = verticalInput * speed;
+        Quaternion rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, Quaternion.LookRotation(aimDirection, Vector3.up).eulerAngles.y,
+            transform.rotation.eulerAngles.z);
 
-			// if moving diagonally
-			if (horizontalInput != 0 && verticalInput != 0)
-				velocity *= Mathf.Cos(45 * Mathf.Deg2Rad);
-			
-			body.velocity = velocity;
+        hand.rotation = rotation;
 
-			isWalking = (horizontalInput != 0 || verticalInput != 0);
-			animator.SetBool("IsWalking", isWalking);
-		}
-	}
-
-	private void faceTowardsMouse()
-	{
-		var directionVector = calculateDirectionToMouse();
-		var x = directionVector.x;
-		var z = directionVector.z;
-
-		Utilities.Direction direction;
-
-		if (x < 0 && Mathf.Abs(x) > Mathf.Abs(z))
-			direction = Utilities.Direction.Left;
-		else if (x > 0 && x > Mathf.Abs(z))
-			direction = Utilities.Direction.Right;
-		else if (z > 0 && z > Mathf.Abs(x))
-			direction = Utilities.Direction.Up;
-		else
-			direction = Utilities.Direction.Down;
-
-		animator.SetInteger("Direction", (int)direction);
-	}
-
-	private Vector3 calculateDirectionToMouse()
-	{
-		var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		var playerPosition = transform.position + new Vector3(centerpointOffset.x, 0, centerpointOffset.y);
-		var direction = (mousePosition - playerPosition).normalized;
-		return direction;
-	}
-
-	private void moveHandTowardsMouse()
-	{
-		Vector3 playerPosition = transform.position + new Vector3(centerpointOffset.x, 0, centerpointOffset.y);
-		Vector3 direction = calculateDirectionToMouse();
-		var handPosition = playerPosition + (direction * handRadius);
-		hand.position = handPosition;
-	}
+        animator.SetInteger("Direction", (int)Utilities.VectorToDirection(direction.x, direction.z));
+    }
 }
