73e6da4da09d3048f7bab5c3c688f6825e6c9df2
bjornt16 <bjornt16@ru.is>
2018-03-19 05:21:03 +0000

    InputManager now does the input handling.
    Improved PlayerControl class to easily check for player movement / aim.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -31,19 +31,24 @@ public class InputManager : SingletonMB<InputManager> {
         //if game is not over and is not paused.
         if (!GameManager.Instance.Paused && !GameManager.Instance.GameOver && UIManager.Instance.openMenu.Count == 0)// && GameManager.Instance.Player.Count > 0)
         {
-            for (int i = 0; i < playerControl.Count; i++)
+            for (int i = 0; i < GameManager.Instance.player.Count; i++)
             {
+                GameManager.Instance.player[i].AimInDirection(playerControl[i].Direction(GameManager.Instance.player[i].transform.position));
+
+                if (playerControl[i].Moving || playerControl[i].Stopped)
+                {
+                    GameManager.Instance.player[i].Move(playerControl[i].Horizontal, playerControl[i].Vertical);
+                }
+
                 //if player i is pressing down his "Shoot" button.
                 if (Input.GetKeyDown(playerControl[i].Shoot))
                 {
-                    //Debug.Log("Player " + i + " Shoot");
-                    //GameManager.Instance.Player[i].Left();
+                    GameManager.Instance.player[i].Shoot();
                 }
                 //if player i is pressing down his "Dash" button.
-                if (Input.GetKeyDown(playerControl[i].Dash))
+                if (playerControl[i].Moving && Input.GetKeyDown(playerControl[i].Dash))
                 {
-                    //Debug.Log("Player " + i + " Dash");
-                    //GameManager.Instance.Player[i].Action();
+                    GameManager.Instance.player[i].Dash();
                 }
             }
         }
@@ -52,8 +57,73 @@ public class InputManager : SingletonMB<InputManager> {
 
 //a struct to organize each players custom inputs. 
 [System.Serializable]
-public struct PlayerControl
+public class PlayerControl
 {
+    const float moveValue = 0.5f;
+
     //these are not final ofcourse.
-    public KeyCode Shoot, Dash, Ability1, Ability2, Ability3;
+    public KeyCode MoveUp, MoveDown, MoveLeft, MoveRight, Shoot, Dash, Ability1, Ability2, Ability3;
+
+    public bool Moving {
+        get
+        {
+            return (Input.GetKey(MoveUp) || Input.GetKey(MoveDown) ||
+                   Input.GetKey(MoveLeft) || Input.GetKey(MoveRight));
+        }
+    }
+
+    public bool Stopped
+    {
+        get
+        {
+            return (Input.GetKeyUp(MoveUp) || Input.GetKeyUp(MoveDown) ||
+                   Input.GetKeyUp(MoveLeft) || Input.GetKeyUp(MoveRight));
+        }
+    }
+
+    public float Horizontal
+    {
+        get
+        {
+            float val = 0;
+            if (Input.GetKey(MoveLeft))
+            {
+                val = -moveValue;
+            }
+            else if (Input.GetKey(MoveRight))
+            {
+                val = moveValue;
+            }
+            return val;
+        }
+    }
+
+    public float Vertical
+    {
+        get
+        {
+            float val = 0;
+            if (Input.GetKey(MoveDown))
+            {
+                val = -moveValue;
+            }
+            else if (Input.GetKey(MoveUp))
+            {
+                val = moveValue;
+            }
+            return val;
+        }
+    }
+
+    private Vector3 Aim{
+        get
+        {
+            return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+    public Vector3 Direction(Vector3 origin)
+    {
+        return (Aim - origin).normalized;
+    }
 }
