2301bd7032dff41ee2647a5870c03eb72acc9a9f
bjornt16 <bjornt16@ru.is>
2018-03-19 05:09:44 +0000

    Moved Direction checking to Utilities.

--- Assets/Scripts/Utilities.cs
+++ Assets/Scripts/Utilities.cs
@@ -1,4 +1,23 @@
-public class Utilities
+using UnityEngine;
+
+public static class Utilities
 {
     public enum Direction { Left, Right, Up, Down }
+
+    public static Direction VectorToDirection(float x, float y)
+    {
+        Direction direction;
+
+        if (x < 0 && Mathf.Abs(x) > Mathf.Abs(y))
+            direction = Utilities.Direction.Left;
+        else if (x > 0 && x > Mathf.Abs(y))
+            direction = Utilities.Direction.Right;
+        else if (y > 0 && y > Mathf.Abs(x))
+            direction = Utilities.Direction.Up;
+        else
+            direction = Utilities.Direction.Down;
+
+        return direction;
+    }
+
 }
