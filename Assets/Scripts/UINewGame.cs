7dcef64f9005b3d28851965ee0a04123db0d1a52
bjornt16 <bjornt16@ru.is>
2018-03-21 07:44:47 +0000

    created newgame menu

---
+++ Assets/Scripts/UINewGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UINewGame : UIMenu
+{
+    public void StartGameButton()
+    {
+        //todo: clean game state
+        SceneLoader.Instance.Load("Overworld scene");
+    }
+
+    public void TogglePlayerCount(int val)
+    {
+        GameManager.Instance.SetPlayerCount(val);
+    }
+
+    public void ReturnButton()
+    {
+        UIManager.Instance.CloseLast();
+    }
+
+}
