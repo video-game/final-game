cee5ae61cb51ba09332884e0a22e11c34a5f3ea7
bjornt16 <bjornt16@ru.is>
2018-03-21 02:17:04 +0000

    Created LevelManager (atm just holds player spawn point)

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : SingletonMB<LevelManager> {
+
+    public List<Transform> StartLocation;
+
+    void OnDrawGizmos()
+    {
+        for (int i = 0; i < StartLocation.Count; i++)
+        {
+            Gizmos.DrawSphere(StartLocation[i].position, 0.25f);
+        }
+    }
+}
