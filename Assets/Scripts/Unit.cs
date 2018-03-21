a5aa0f839e86ea9bfd55fd4872d8365b9112b263
bjornt16 <bjornt16@ru.is>
2018-03-21 06:44:15 +0000

    created base unit class (this is where basic stuff like hp goes)

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour {
+
+    public delegate void StatChangeDelegate(int current, int max);
+    public StatChangeDelegate OnHealthChange;
+
+    protected int maxHealth;
+    protected int currentHealth;
+
+    public void ChangeHealth(int value)
+    {
+        currentHealth = (currentHealth + value) < 0 ? 0 : (currentHealth + value) > maxHealth ? maxHealth : (currentHealth + value);
+
+        Debug.Log(currentHealth);
+
+        if (OnHealthChange != null)
+        {
+            OnHealthChange(currentHealth, maxHealth);
+        }
+    }
+
+}
