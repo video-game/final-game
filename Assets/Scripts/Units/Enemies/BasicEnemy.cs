574e228903d4483d0fa879c88e921cc0b64a2bac
bjornt16 <bjornt16@ru.is>
2018-04-03 12:36:01 +0000

    added line of sight checking to enemy  (uses nav mesh) .. so far only basic enemy uses it.

--- Assets/Scripts/Units/Enemies/BasicEnemy.cs
+++ Assets/Scripts/Units/Enemies/BasicEnemy.cs
@@ -36,13 +36,13 @@ public class BasicEnemy : Enemy
         animator.SetBool("IsWalking", agent.velocity != Vector3.zero);
         animator.SetInteger("Direction", (int)Utilities.VectorToDirection(agent.velocity.x, agent.velocity.z));
 
-        //NOTE: this code assumes that there is 1 player only. Will need fixing if we do 2 player.
         Vector3 playerPosition = GetClosestPlayer();
 
         if (playerPosition != Vector3.zero && attacking)
-        {
+        { 
+    
             shootTimer += Time.deltaTime;
-            if(shootTimer > shootSpeed)
+            if(shootTimer > shootSpeed && HasLineOfSight(playerPosition))
             {
                 shootTimer = 0;
 
@@ -50,22 +50,6 @@ public class BasicEnemy : Enemy
 
                 GameObject clone = Instantiate(bullet, new Vector3(transform.position.x, 0, transform.position.z), bullet.transform.rotation);
                 clone.GetComponent<Projectile>().Init((playerPosition - zeroedPos).normalized * 4, gameObject);
-
-                //TODO make this work
-                //for some reason raycasting on the mesh doesn't work so we just have to make it shoot at blockades I guess
-
-                //The NavMeshAgent tends to set the y value to something not quite 0,
-                //and the environment mesh is only exactly at y=0.
-                //So we must fix this by raycasting a y zeroed vector.
-                /*
-                RaycastHit hit;
-                Debug.DrawRay(zeroedPos, (playerPosition - zeroedPos), Color.red, playerCheckTime);
-                
-                if (Physics.Raycast(zeroedPos, (playerPosition - zeroedPos), out hit))
-                {
-                    Debug.Log("hit something! " + hit.transform);
-                }
-                */
             }
         }
 
