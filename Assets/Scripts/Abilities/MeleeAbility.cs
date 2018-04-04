1429842d307a5715859cd75d6c9289283c4fac47
bjornt16 <bjornt16@ru.is>
2018-04-04 10:58:03 +0000

    Melee / ranged specific abilities

---
+++ Assets/Scripts/Abilities/MeleeAbility.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Ability/Melee", fileName = "New Melee Ability")]
+public class MeleeAbility : Ability {
+
+    public float distance;
+    public float degrees;
+    public MeleeAbilityHitDetector MAHD;
+
+    public void OnUse()
+    {
+        MeleeAbilityHitDetector MAHD = new GameObject().AddComponent< MeleeAbilityHitDetector>();
+        MAHD.Init(this, "Enemy" , UsedBy.AimDirection);
+        base.OnUse(MAHD);
+    }
+
+}
---
+++ Assets/Scripts/Abilities/RangedAbility.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Ability/Ranged", fileName = "New Ranged Ability")]
+public class RangedAbility : Ability {
+
+    public Sprite projectileSprite;
+    public float speed;
+    public float spread;
+    public float lifeTime;
+
+    [HideInInspector]
+    public RangedAbilityHitDetector RAHD;
+
+    public override void Init(Unit u)
+    {
+        base.Init(u);
+    }
+
+    public void OnUse()
+    {
+        if (ready)
+        {
+            GameObject temp = new GameObject();
+            RAHD = temp.AddComponent<RangedAbilityHitDetector>();
+            RAHD.Init(this, "Enemy", UsedBy.AimDirection, lifeTime);
+            
+            base.OnUse(RAHD);
+        }
+    }
+
+}
