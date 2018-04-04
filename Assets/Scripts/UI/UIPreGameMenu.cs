b443ff7cff78a63bf5b30b60c0e4b66c594c097e
bjornt16 <bjornt16@ru.is>
2018-04-04 17:49:12 +0000

    Menu for Valli "PreGameMenu"

---
+++ Assets/Scripts/UI/UIPreGameMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPreGameMenu : UIMenu
+{
+    public void ContinueButton()
+    {
+        SceneLoader.Instance.Load("Overworld scene");
+    }
+
+}
