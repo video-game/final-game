46ee4ad04cd769eadb220bcd68cbf34c05f85cea
bjornt16 <bjornt16@ru.is>
2018-04-04 10:58:52 +0000

    some starter effects. (used in abilities)

---
+++ Assets/Scripts/Abilities/Effects/AnimationEffect.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Ability/Effect/AnimationOnHit", fileName = "New Animation OnHit Effect")]
+public class AnimationEffect : Effect
+{
+    public bool loop;
+
+    public bool destroyOnAnimEnd;
+
+    public bool stopMovementOnStart;
+
+    public List<AnimEffect> animList;
+
+    AbilityHitDetector hitDetector;
+
+    public override void Trigger(AbilityHitDetector hitWith)
+    {
+        hitDetector = hitWith;
+        hitDetector.ability.slave.StartCoroutine(PlayAnimation());
+    }
+
+    public override void Trigger(AbilityHitDetector hitWith, GameObject hitTarget)
+    {
+        hitDetector = hitWith;
+        hitDetector.ability.slave.StartCoroutine(PlayAnimation());
+    }
+
+    protected IEnumerator PlayAnimation()
+    {
+        SpriteRenderer sr = hitDetector.GetComponent<SpriteRenderer>();
+
+        if (stopMovementOnStart)
+        {
+            hitDetector.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+
+        do
+        {
+            for (int i = 0; i < animList.Count; i++)
+            {
+                sr.sprite = animList[i].Sprite;
+                yield return new WaitForSeconds(animList[i].duration);
+                if(sr == null)
+                {
+                    break;
+                }
+            }
+
+        } while (loop);
+
+        if (sr == null != destroyOnAnimEnd)
+        {
+            hitDetector.Destroy();
+        }
+    }
+
+}
+
+[System.Serializable]
+public struct AnimEffect
+{
+    public Sprite Sprite;
+    [Range(0.1f, 3f)]
+    public float duration;
+}
---
+++ Assets/Scripts/Abilities/Effects/Effect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Effect : ScriptableObject {
+
+    public virtual void Trigger() { }
+
+    public virtual void Trigger(AbilityHitDetector hitWith, GameObject hit) { }
+
+    public virtual void Trigger(AbilityHitDetector hitWith) { }
+
+    public virtual void Trigger(Ability ability) { }
+
+}
