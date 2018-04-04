d594ef5dbe678ab614497aeebadba8edaa4944c8
bjornt16 <bjornt16@ru.is>
2018-04-04 15:28:23 +0000

    Ability bar scripts and prefabs

---
+++ Assets/Scripts/Abilities/AbilityBar.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityBar : MonoBehaviour {
+
+    private Player player;
+    public List<AbilityBarButton> abilities;
+
+    public void Init(Player p)
+    {
+        player = p;
+
+        player.OnNewAbility += UpdateAbilityBar;
+        player.OnAbilityUsed += Click;
+        player.OnAbilitySelected += Select;
+    }
+
+    public void Click(int index)
+    {
+        abilities[index].Click();
+    }
+
+    public void Select(int index)
+    {
+        player.selectedAbilityIndex = index;
+        for (int i = 0; i < abilities.Count; i++)
+        {
+            if(i == index)
+            {
+                abilities[i].selected.gameObject.SetActive(true);
+            }
+            else
+            {
+                abilities[i].selected.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void UpdateAbilityBar(List<RangedAbility> newList)
+    {
+        Debug.Log(newList.Count);
+        for (int i = 0; i < newList.Count; i++)
+        {
+            if(newList[i] != null)
+            {
+                abilities[i].enabled = true;
+                abilities[i].Init(newList[i]);
+            }
+            else
+            {
+                abilities[i].enabled = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Abilities/AbilityBarButton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityBarButton : MonoBehaviour {
+
+    public UnityEngine.UI.Button button;
+    public UnityEngine.UI.Image AbilityImage;
+    public UnityEngine.UI.Image CoolDownFade;
+    public UnityEngine.UI.Image selected;
+
+    private RangedAbility ability;
+    bool cd = false;
+
+    public void Init(RangedAbility ability)
+    {
+        this.ability = ability;
+        ability.ready = true;
+        if(ability.AbilityBarImage != null)
+        {
+            AbilityImage.sprite = ability.AbilityBarImage;
+            CoolDownFade.sprite = ability.AbilityBarImage;
+        }
+    }
+
+    public void Click()
+    {
+        if (!cd)
+        {
+            StartCoroutine(CoolDown(Mathf.Max(ability.coolDownRemaining, ability.cooldown)));
+        }
+
+    }
+
+    IEnumerator CoolDown(float cooldown)
+    {
+        cd = true;
+        CoolDownFade.fillAmount = 1;
+
+        var currentPos = transform.position;
+        float t = 0f;
+        while (t < 1)
+        {
+            t += Time.deltaTime / cooldown;
+            CoolDownFade.fillAmount = Mathf.Lerp(1f, 0f, t);
+            yield return null;
+        }
+        cd = false;
+    }
+}
